e3937c5e147dce48a0c0fbf83f82d352b8ff47ab
Костя Исаченко <kostayisacenko@MacBook-Pro-Kosta.local>
2021-01-31 19:59:40 +0300

    Models saved

--- store_project/proj/book/admin.py
+++ store_project/proj/book/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author
-from .models import Series
-from .models import Genre
-from .models import Publishing_house
+from . models import Author
+from . models import Series
+from . models import Genre
+from . models import Publishing_house
 
 admin.site.register(Author)
 
