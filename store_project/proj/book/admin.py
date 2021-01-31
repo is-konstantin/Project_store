01e2caedc2733f3c4ccb28d4ac77bb8ccfc8f0c1
Костя Исаченко <kostayisacenko@MacBook-Pro-Kosta.local>
2021-01-31 17:16:13 +0300

    Second time added

--- store_project/proj/book/admin.py
+++ store_project/proj/book/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author
+from .models import Series
+from .models import Genre
+from .models import Publishing_house
+
+admin.site.register(Author)
+
+admin.site.register(Series)
+
+admin.site.register(Genre)
+
+admin.site.register(Publishing_house)
+
+
+
--- store_project/proj/book/models.py
+++ store_project/proj/book/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    author_name = models.CharField("Name", max_length = 100)
+    author_description = models.CharField("Description", max_length = 400)
+    def __str__(self):
+        return self.author_name
+
+class Series(models.Model):
+    series_name = models.CharField("Name", max_length = 100)
+    series_description = models.CharField("Description", max_length = 400)
+    def __str__(self):
+        return self.series_name
+
+class Genre(models.Model):
+    genre_name = models.CharField("Name", max_length = 100)
+    genre_description = models.CharField("Description", max_length = 400)
+    def __str__(self):
+        return self.author_name
+
+class Publishing_house(models.Model):
+    publishing_house_name = models.CharField("Name", max_length = 100)
+    publishing_house_description = models.CharField("Description", max_length = 400)
+    def __str__(self):
+        return self.publishing_house_name
+
