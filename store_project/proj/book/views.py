d35d73769f418375900cc467b4588b8265365045
Костя Исаченко <kostayisacenko@MacBook-Pro-Kosta.local>
2021-02-04 13:59:25 +0300

    Added html with hello

--- store_project/proj/book/views.py
+++ store_project/proj/book/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from book.models import Author
 
 # Create your views here.
+def home_page(request):
+    author = Author.objects.first()
+    contxt = {"author" : author}
+    return render(request, template_name = "home.html", context = contxt)
+    #html = "Authors: <br>"
+    #for auth in authors:
+    #    html += f"{auth.author_name} <br>"
+    #return HttpResponse(html)
